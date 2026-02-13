c657e02962f165a578d047121154dc1cbb6dc56e
corvohq <dev@corvohq.com>
2026-02-13 00:12:47 -0700

    phase5: add SSE endpoint for per-job stream deltas

--- internal/server/server.go
+++ internal/server/server.go
@@ -85,6 +85,7 @@ func (s *Server) buildRouter() chi.Router {
 		r.Get("/queues", s.handleListQueues)
 		r.Get("/jobs/{id}", s.handleGetJob)
 		r.Get("/jobs/{id}/iterations", s.handleListJobIterations)
+		r.Get("/jobs/{id}/stream", s.handleJobStreamSSE)
 		r.Post("/jobs/search", s.handleSearch)
 		r.Get("/workers", s.handleListWorkers)
 		r.Get("/cluster/status", s.handleClusterStatus)
